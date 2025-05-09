044cca226ae9ea60cf1fb4cc13542a51308ea5bd
HaseebSardar286 <haseebsardar286@gmail.com>
2025-05-09 14:49:29 +0500

    initial commit

---
+++ src/app/planogram/planogram.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-planogram',
+  imports: [],
+  templateUrl: './planogram.component.html',
+  styleUrl: './planogram.component.css'
+})
+export class PlanogramComponent {
+
+}
